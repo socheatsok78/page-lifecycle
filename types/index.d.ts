62a4fefd4138f61f50b54fd5af961513aef65e08
Socheat Sok <alex@socheat.net>
2021-09-28 14:22:03 +0700

    feat: add typescript decoration files

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+import Lifecycle from "./Lifecycle";
+
+/**
+ * Original work by @magic-akari
+ * @see https://github.com/GoogleChromeLabs/page-lifecycle/pull/12
+ */
+declare const _default: Lifecycle;
+export default _default;
